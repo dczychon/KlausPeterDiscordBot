f56d9619b5b27c75e3dc9740297724caa3f8ed37
dczychon <d.czychon@outlook.com>
2019-03-02 21:42:53 +0100

    [WIP] added tests for help text renderer

---
+++ test/DiscordBot.Util.Tests/HelpTextRendererTests.cs
@@ -0,0 +1,20 @@
+﻿using DiscordBot.Util.Commands;
+using DiscordBot.Util.Commands.Predefined;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DiscordBot.Util.Tests
+{
+    public class HelpTextRendererTests
+    {
+        [Theory]
+        [InlineData(typeof(PingCommand), "some text")]
+        public void RenderSyntaxHelpTest(Type inputType, string expectedSyntaxHelp)
+        {
+            HelpTextRenderer helpTextRenderer = new HelpTextRenderer(inputType);
+            Assert.Equal(expectedSyntaxHelp, helpTextRenderer.GenerateSyntaxHelp().ToString());
+        }
+    }
+}
