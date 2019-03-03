895f7bb87c50d1e64e7410d8a87836e9c3042de8
dczychon <d.czychon@outlook.com>
2019-03-03 14:12:35 +0100

    added tests for some help text renderer methods

--- test/DiscordBot.Util.Tests/HelpTextRendererTests.cs
+++ test/DiscordBot.Util.Tests/HelpTextRendererTests.cs
@@ -1,5 +1,6 @@
 ﻿using DiscordBot.Util.Commands;
 using DiscordBot.Util.Commands.Predefined;
+using DiscordBot.Util.RuntimeDebugging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,11 +11,23 @@ namespace DiscordBot.Util.Tests
     public class HelpTextRendererTests
     {
         [Theory]
-        [InlineData(typeof(PingCommand), "some text")]
+        [InlineData(typeof(PingCommand), "Syntax: **!ping**")]
+        [InlineData(typeof(HelpCommand), "Syntax: **!help** (Command name)")]
+        [InlineData(typeof(InspectDscCommand), "Syntax: **!inspect-dsc** [Path]")]
         public void RenderSyntaxHelpTest(Type inputType, string expectedSyntaxHelp)
         {
             HelpTextRenderer helpTextRenderer = new HelpTextRenderer(inputType);
             Assert.Equal(expectedSyntaxHelp, helpTextRenderer.GenerateSyntaxHelp().ToString());
+        }
+
+        [Theory]
+        [InlineData(typeof(PingCommand), "Syntax: **!ping**")]
+        [InlineData(typeof(HelpCommand), "Syntax: **!help** (Command name)")]
+        [InlineData(typeof(InspectDscCommand), "Syntax: **!inspect-dsc** [Path]")]
+        public void RenderArgumentsHelp(Type inputType, string expectedArgumentsHelp)
+        {
+            HelpTextRenderer helpTextRenderer = new HelpTextRenderer(inputType);
+            Assert.Equal(expectedArgumentsHelp, helpTextRenderer.GenerateArgumentsHelp().ToString());
         }
     }
 }
