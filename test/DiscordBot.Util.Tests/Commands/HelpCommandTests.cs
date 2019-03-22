0be833c8ee53a09d69fabe0909bd6a5da7bd39ff
Dominik Czychon <dominikc99@gmail.com>
2019-03-22 21:04:46 +0100

    Added more tests and editorconfig (#12)
    * [WIP] added tests for help text renderer
    
    * added tests for some help text renderer methods
    
    * finalized tests for help renderer
    
    * fixed tests
    
    * added tests for HelpCommand
    
    * added private command tests
    
    * added editorconfig files

---
+++ test/DiscordBot.Util.Tests/Commands/HelpCommandTests.cs
@@ -0,0 +1,83 @@
+﻿using Discord;
+using DiscordBot.Util.Commands;
+using DiscordBot.Util.Commands.Predefined;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace DiscordBot.Util.Tests.Commands
+{
+    public class HelpCommandTests
+    {
+        [Fact]
+        public async Task RendersHelpForAllCommandInCommandContainerTest()
+        {
+            string expectedOutput = @":information_source: __**Here are all supported commands:**__ :information_source:
+
+**!help** 	- Prints usage for a command
+**!ping** 	- Execute this to get a super special response
+";
+
+            ICommandContainer container = new CommandContainer();
+            container.AddCommandHandler<PingCommand>();
+            container.AddCommandHandler<HelpCommand>();
+
+            string respone = null;
+
+            Mock<IMessage> socketMessageMock = new Mock<IMessage>();
+            Mock<IMessageChannel> messageChannelMock = new Mock<IMessageChannel>();
+            messageChannelMock
+                .Setup(c => c.SendMessageAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<Embed>(), It.IsAny<RequestOptions>()))
+                .Callback<string, bool, Embed, RequestOptions>((msg, tts, embed, reqOptions) => respone = msg)
+                .Returns(() => Task.FromResult<IUserMessage>(default(SocketUserMessage)));
+            socketMessageMock
+                .Setup(x => x.Channel)
+                .Returns(messageChannelMock.Object);
+
+            HelpCommand helpCommand = new HelpCommand(container);
+
+            //Act
+            await helpCommand.Execute(socketMessageMock.Object);
+
+            //Assert
+            messageChannelMock.Verify(c => c.SendMessageAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<Embed>(), It.IsAny<RequestOptions>()), Times.Once());
+            Assert.Equal(expectedOutput, respone);
+        }
+
+        [Fact]
+        public async Task ShowErrorIfHelpForUnknownCommandIsRequested()
+        {
+            string expectedOutput = "\"foo\" is an unknown command. Type !help to list all available commands!";
+
+            ICommandContainer container = new CommandContainer();
+            container.AddCommandHandler<PingCommand>();
+
+            string respone = null;
+
+            //TODO Write a helper method to create this mock, because we use the same code in a lot of tests to create this basic mock
+            Mock<IMessage> socketMessageMock = new Mock<IMessage>();
+            Mock<IMessageChannel> messageChannelMock = new Mock<IMessageChannel>();
+            messageChannelMock
+                .Setup(c => c.SendMessageAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<Embed>(), It.IsAny<RequestOptions>()))
+                .Callback<string, bool, Embed, RequestOptions>((msg, tts, embed, reqOptions) => respone = msg)
+                .Returns(() => Task.FromResult<IUserMessage>(default(SocketUserMessage)));
+            socketMessageMock
+                .Setup(x => x.Channel)
+                .Returns(messageChannelMock.Object);
+
+            HelpCommand helpCommand = new HelpCommand(container);
+            helpCommand.Command = "foo";
+
+            //Act
+            await helpCommand.Execute(socketMessageMock.Object);
+
+            //Assert
+            messageChannelMock.Verify(c => c.SendMessageAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<Embed>(), It.IsAny<RequestOptions>()), Times.Once());
+            Assert.Equal(expectedOutput, respone);
+        }
+    }
+}
---
+++ test/DiscordBot.Util.Tests/HelpTextRendererTests.cs
@@ -0,0 +1,39 @@
+﻿using DiscordBot.Util.Commands;
+using DiscordBot.Util.Commands.Predefined;
+using DiscordBot.Util.RuntimeDebugging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DiscordBot.Util.Tests
+{
+    public class HelpTextRendererTests
+    {
+        [Theory]
+        [InlineData(typeof(PingCommand), "Syntax: **!ping**")]
+        [InlineData(typeof(HelpCommand), "Syntax: **!help** (Command name)")]
+        [InlineData(typeof(InspectDscCommand), "Syntax: **!inspect-dsc** [Path]")]
+        public void RenderSyntaxHelpTest(Type inputType, string expectedSyntaxHelp)
+        {
+            HelpTextRenderer helpTextRenderer = new HelpTextRenderer(inputType);
+            Assert.Equal(expectedSyntaxHelp, helpTextRenderer.GenerateSyntaxHelp().ToString());
+        }
+
+        [Theory]
+        [InlineData(typeof(PingCommand), "**!ping** \t- Execute this to get a super special response")]
+        [InlineData(typeof(HelpCommand), "**!help** \t- Prints usage for a command")]
+        [InlineData(typeof(InspectDscCommand), "**!inspect-dsc** \t- Allows the inspection of the DiscordSocketClient at runtime")]
+        public void RenderHeaderHelpTest(Type inputType, string expectedHeader)
+        {
+            HelpTextRenderer helpTextRenderer = new HelpTextRenderer(inputType);
+            Assert.Equal(expectedHeader, helpTextRenderer.GenerateHeaderHelp().ToString());
+        }
+
+        [Fact]
+        public void DontRenderHelpForTypesWithoutCommandVerbAttribute()
+        {
+            Assert.Throws<ArgumentException>(()=> new HelpTextRenderer(typeof(String)).GetHelp());
+        }
+    }
+}
